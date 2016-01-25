2466d294973020c7c74fcf994602553510652c48
LandoB <jrolandobernal@gmail.com>
2016-01-25 09:29:02 -0600

    Getting Ryu Out of The Hadouken Pose and Adding the Hadouken Sound

--- js/app.js
+++ js/app.js
@@ -1,11 +1,58 @@
-$('#my-div').append('<img src="img/90Mmdcm.png">')
+$( document ).ready(function() {
+		 // jQuery project demo
+		var playHadouken = false;
 
-$('#my-div img').hover( function() {
-    this.src = 'img/nTj3Fxx.gif'
-}, function() {
-    this.src = 'img/90Mmdcm.png'
-})
+		$('#my-div').append('<img src="img/90Mmdcm.png">')
 
-$('#my-div img').mousedown( function() {
-    this.src = 'img/Rfj0a80.png'
-})
+		$('#my-div img').hover( function() {
+				this.src = 'img/nTj3Fxx.gif'
+		}, function() {
+				this.src = 'img/90Mmdcm.png'
+		})
+
+		$('#my-div img').mousedown( function() {
+				this.src = 'img/Rfj0a80.png'
+		})
+
+		$('#my-div img').mousedown(function() {
+				$('.demo-hadouken').remove();
+		})
+
+		$('#my-div img').mousedown(function() {
+				$('#my-div').append(
+				'<img class="demo-hadouken" src="img/oTyQRvX.gif">'
+				);
+		})
+
+		$('#my-div img').mousedown(function() {
+			if (playHadouken) {
+				$('#hadouken-sound')[0].load();
+				$('#hadouken-sound')[0].play();
+			}
+				$('.demo-hadouken').animate( {
+						"margin-left": "600px"
+				}, 1000, 'swing', function() {
+						this.remove();
+				})
+		})
+
+
+		$('#my-div img').mouseup(function() {
+				this.src = 'img/90Mmdcm.png'
+		})
+
+		$('#main-example').on('mouseup', function() {
+				this.src = '/learn/static/guides/intro-to-jquery/images/ryu_animated.gif';
+			});
+
+			$('#main-example-add-sound').on('click', function() {
+				playHadouken = !playHadouken;
+
+				if (playHadouken)
+					$(this).html('Sound added, click on Ryu');
+
+				if (!playHadouken)
+					$(this).html('Sound removed, click to add it back');
+		});
+
+});
