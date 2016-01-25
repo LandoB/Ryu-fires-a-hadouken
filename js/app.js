187e67059e922f18c8c06ef6250e3560f68bc19d
LandoB <jrolandobernal@gmail.com>
2016-01-25 08:43:04 -0600

    Added Ryu's hadouken pose

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
 $('#my-div').append('<img src="img/90Mmdcm.png">')
+
+$('#my-div img').hover( function() {
+    this.src = 'img/nTj3Fxx.gif'
+}, function() {
+    this.src = 'img/90Mmdcm.png'
+})
+
+$('#my-div img').mousedown( function() {
+    this.src = 'img/Rfj0a80.png'
+})
